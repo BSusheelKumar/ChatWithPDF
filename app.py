3e7c0892394602c8bff32ee4505f46312e730be8
Badikala Susheel Kumar <bsusheelkumar01@gmail.com>
2024-04-19 09:05:29 +0530

    made changes

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def get_vector_store(text_chunks):
 def get_conversational_chain():
 
     prompt_template = """
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    You are a PDF reader,Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
     provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
     Context:\n {context}?\n
     Question: \n{question}\n
